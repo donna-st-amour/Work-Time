761ce237293cf2acff10e4f35c8b698fad501a59
donna-st-amour <donna.st.amour@gmail.com>
2020-01-10 11:52:01 -0600

    Version 2
    Added day names, and changed variables to arrays to store entire week of information. Allowed for printout of entire week.

--- WorkHours.java
+++ WorkHours.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
 public class WorkHours {
-static int maxHours;
-static int maxMins;
-static int startTime;
-static int lunchStart;
-static int lunchEnd;
-static int lunchTime;
-static int endTime;
-static int remainTime;
-static int dayTime;
-
-
+	
 	public static void main(String[] args) {
+		int maxHours;
+		int maxMins;
+		int[] startTime = new int[5];
+		int[] lunchStart = new int[5];
+		int[] lunchEnd = new int[5];
+		int[] lunchTime = new int[5];
+		int[] endTime = new int[5];
+		int remainTime;
+		int[] dayTime = new int[5];
+		int totalTimeWorked = 0;
+		String day[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+		
 		Scanner keyboard = new Scanner(System.in);
 		
 		//Assumes that the number of allowable hours per week is an integer value
@@ -26,12 +28,12 @@ static int dayTime;
 		int numDaysWorked = keyboard.nextInt();
 		
 		//Loop to contain input values
-		for(int i = 1; i <= numDaysWorked; i++) {
-			System.out.println("Please enter starting hour for day " + i + " (00 to 23)");
+		for(int i = 0; i < numDaysWorked; i++) {
+			System.out.println("Please enter starting hour for " + day[i] + " (00 to 23)");
 			int startHr = keyboard.nextInt(); 
 			System.out.println("Please enter the starting minute for day " + i );
 			int startMin = keyboard.nextInt();
-			startTime = (startHr * 60) + startMin;
+			startTime[i] = (startHr * 60) + startMin;
 			
 			//Determine if lunch was taken and how much time it was
 			keyboard.nextLine();
@@ -42,20 +44,20 @@ static int dayTime;
 				int lunchStartHr = keyboard.nextInt();
 				System.out.println("Please enter the starting minutes of your lunch: ");
 				int lunchStartMin = keyboard.nextInt();
-				lunchStart = (lunchStartHr * 60) + lunchStartMin;
+				lunchStart[i] = (lunchStartHr * 60) + lunchStartMin;
 				
 				System.out.println("Please enter the ending hour of your lunch: ");
 				int lunchEndHr = keyboard.nextInt();
 				System.out.println("Please enter the ending minutes of your lunch: ");
 				int lunchEndMin = keyboard.nextInt();
-				lunchEnd = (lunchEndHr * 60) + lunchEndMin;
+				lunchEnd[i] = (lunchEndHr * 60) + lunchEndMin;
 				
-				lunchTime = lunchEnd - lunchStart;
+				lunchTime[i] = lunchEnd[i] - lunchStart[i];
 			}//end if
 			
 			//If no lunch was taken, lunch time used is zero
 			else {
-				lunchTime = 0;
+				lunchTime[i] = 0;
 			}//end else
 			
 			//Determine end of work day time and calculate time worked for entire day.
@@ -63,15 +65,18 @@ static int dayTime;
 			int quitHour = keyboard.nextInt();
 			System.out.println("Please enter the quitting minutes for the day: ");
 			int quitMin = keyboard.nextInt();
-			endTime = (quitHour * 60) + quitMin;
-			dayTime = endTime - startTime - lunchTime;
-			//System.out.println("Your worked " + dayTime/60 + " hours " + dayTime % 60 + " minutes on day " + i);
+			endTime[i] = (quitHour * 60) + quitMin;
+			dayTime[i] = endTime[i] - startTime[i] - lunchTime[i];
 			
 			//Calculate how much time can be worked in remainder of week.
-			remainTime -= dayTime;
+			remainTime -= dayTime[i];
 		}//end for
 		
 		//Print results
+		for(int j = 0; j < 5; j++) {
+			System.out.println("On " + day[j] +", you worked " + dayTime[j]/60 + " hours and " + dayTime[j] % 60 + " minutes.");
+			System.out.println();
+		}//end for
 		System.out.println("Time remaining for the week is: " + remainTime/60 + " Hours and " + remainTime % 60 + " Minutes.");
 		keyboard.close();
 	}//end main method
